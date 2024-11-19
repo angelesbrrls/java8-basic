ab8140b1bb189b04155d7e8710c974473287f16e
angelesbrrls <a.brrls@gmail.com>
2024-11-18 19:06:24 -0600

    practices Map and Filter

--- src/streams/practices/MapFilter2.java
+++ src/streams/practices/MapFilter2.java
@@ -1,5 +1,24 @@
 package streams.practices;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*
+ * De una lista de números enteros:
+ * Filtra los números que sean mayores a 10.
+ * Aplica un map para restarles 5 a los números filtrados.
+ * Obtén el resultado como una lista.
+*/
 public class MapFilter2 {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> numeros = Arrays.asList(23, 35, 43, 11, 33, 22, 7, 9 ,5, 34);
+		
+		List<Integer> resultado = numeros.stream().filter(n -> n > 10).map(n -> n -5).collect(Collectors.toList());
+		
+		resultado.forEach(System.out::println);
+	}
 
 }
